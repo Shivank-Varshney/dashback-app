102a057bb9a2f17e6ddddcdec4b89bd78896542a
Shivank Varshney <shivank1703@gmail.com>
2021-05-25 19:06:27 +0530

    data shows in bg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'dash',
+    loadChildren: () => import('./dash/dash.module').then( m => m.DashPageModule)
+  },
 ];
 
 @NgModule({
