283ac008b8e3a36c8af17340d62bcaf791eb3d1b
Shivank Varshney <shivank1703@gmail.com>
2021-05-26 16:31:19 +0530

    otp and pin keypad done

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
-  slide = '10vh';
+  slide = '8vh';
   trans = '1s';
     constructor() {}
   
@@ -14,6 +14,6 @@ export class LoginPage {
       this.slide = '80vh'
     }
     slideDown(){
-      this.slide = '10vh'
+      this.slide = '8vh'
     }
 }
